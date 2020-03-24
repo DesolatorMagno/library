1394ee89e37c8c44665688b18520f4372f2aeddd
Reny Ramos <renyramosgarcia@gmail.com>
2020-03-23 23:12:02 -0400

    Agregado Rutas Books

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/books', 'BooksController@store')->name('books.store');
 Route::patch('/books/{book}', 'BooksController@update')->name('books.update');
+Route::delete('/books/{book}', 'BooksController@destroy')->name('books.destroy');
+Route::get('/books', 'BooksController@index')->name('books.index');
+Route::get('/books/{book}', 'BooksController@show')->name('books.show');
